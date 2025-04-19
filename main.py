e794cda8dc4f92168b1efa53fe0db6535ecefa0e
Okan345 <okanekici3@gmail.com>
2025-04-19 13:42:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
 from flask import Flask, request
+from flask_cors import CORS
 import requests
 import os
-from flask_cors import CORS  # 🆕 CORS modülü eklendi
 
 app = Flask(__name__)
-CORS(app)  # 🆕 CORS aktif edildi
-
-app = Flask(__name__, static_folder='public', static_url_path='/public')
-
+CORS(app)
 
 @app.route("/update", methods=["GET"])
 def update():
     data1 = request.args.get("data1", default="0")
     data2 = request.args.get("data2", default="0")
-
-    # Wuaze (000Webhost) adresine yönlendirme
     wuaze_url = f"http://sayac-takip.wuaze.com/update.php?data1={data1}&data2={data2}&i=1"
-
     try:
-        headers = {
-            "User-Agent": "Mozilla/5.0"
-        }
+        headers = {"User-Agent": "Mozilla/5.0"}
         response = requests.get(wuaze_url, headers=headers, timeout=5)
         response.raise_for_status()
     except Exception as e:
         return f"Wuaze aktarım hatası: {e}"
-
     return f"Render veri alındı ve wuaze'e yönlendirildi: {data1}, {data2}"
 
 @app.route("/sensor_data.txt")
